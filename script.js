2735c58979ee59ffa8cc0e7fb0da0d36a1cadeab
John Butler <johnmichaelbutler@yahoo.com>
2020-06-28 15:29:07 -0500

    Inclded api info in our script.js. Created our getPhotos function.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('testing');
+// Unsplash API
+const count = 10;
+const apiKey = `Zp3ysY49on6x_rtJWpi4h2LfbTjyn0T1dRCpz5padMA`;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function gePhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch {
+    // Catch Error here
+    console.log(Error);
+  }
+}
+
+// On Load
+gePhotos();
